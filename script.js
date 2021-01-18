d64b449f1d2cce1c8df585a2680363aa5de8425e
jackegan444 <jack.egan444@gmail.com>
2021-01-18 15:00:12 -0500

    Add draw functionality with changing pen colors

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const gridContainer = document.querySelector('#grid-container');
+const sizeInput = document.querySelector('#size-input')
+const sizeBtn = document.querySelector('#size-btn');
+const colorBtns = document.getElementsByClassName("color-button");
 const sketchGrid = [];
+let penColor = 'black'
 
-for (let i = 0; i < 16; i++) {
-    sketchGrid.push([]);
-    for (let j = 0; j < 16; j++) {
-        sketchGrid[i].push(document.createElement('div'));
-        sketchGrid[i][j].classList.add('grid-space');
-        sketchGrid[i][j].style.cssText = `grid-column: ${j + 1}; grid-row: ${i + 1}`;
-        sketchGrid[i][j].textContent = 'yep';
-        gridContainer.appendChild(sketchGrid[i][j]);
+function changePenColor(e) {
+    penColor = e.target.getAttribute('data-color');
+}
+
+function changeTileColor(e) {
+    e.target.style.cssText = `background: ${penColor}`;
+}
+
+function setGridSize(e) {
+    gridContainer.innerHTML = '';
+    sketchGrid.length = 0;
+    const gridSize = sizeInput.value;
+    for (let i = 0; i < gridSize; i++) {
+        sketchGrid.push([]);
+        for (let j = 0; j < gridSize; j++) {
+            sketchGrid[i].push(document.createElement('div'));
+            sketchGrid[i][j].classList.add('grid-space');
+            sketchGrid[i][j].style.cssText = `grid-column: ${j + 1}; grid-row: ${i + 1}`;
+            sketchGrid[i][j].addEventListener('mouseover', changeTileColor);
+            gridContainer.appendChild(sketchGrid[i][j]);
+        }
     }
 }
+
+for (let i = 0; i < colorBtns.length; i++) {
+    colorBtns[i].style.cssText = `background-color: ${colorBtns[i].getAttribute('data-color')}`;
+    colorBtns[i].addEventListener('click', changePenColor);
+}
+
+sizeBtn.addEventListener('click', setGridSize);
+setGridSize();
